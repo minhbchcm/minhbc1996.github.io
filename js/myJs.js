2200eb801ea634aeb90b96bd06d3165620f572e5
minhthps03339 <minhbc1996@gmail.com>
2022-10-12 14:24:58 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://fb.com/nguyenphan299";
+            window.location = "https://minhhcm96.github.io/index.html";
           },
         });
       }
